97bdfa4473429c9de492a097b3584efbd36fed8a
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2024-06-14 19:14:39 +0700

    Fix toast animation

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -4,6 +4,7 @@ import apiClient from "../lib/apiClient";
 import { LoginRequest, LoginResponse } from "@/types/auth.type";
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
+import { toast } from "@/components/ui/use-toast";
 
 const fetchLogin = async (credentials: LoginRequest) => {
   const response = await apiClient.post<LoginResponse>(
@@ -23,6 +24,11 @@ const useLogin = () => {
       const accessToken = Cookies.get("accessToken") || "";
       const refreshToken = Cookies.get("refreshToken") || "";
       setTokens(accessToken, refreshToken);
+
+      toast({
+        variant: "success",
+        description: "Login successful",
+      });
 
       router.replace("/dashboard");
     },
