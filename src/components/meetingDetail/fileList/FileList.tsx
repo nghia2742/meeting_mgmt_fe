bc9078ecc2543cb3a161890a7d1b08266fac27d8
unknown <badatdoan@gmail.com>
2024-06-16 10:08:48 +0700

    [File] - Add filter by type and name

--- src/components/meetingDetail/fileList/FileList.tsx
+++ src/components/meetingDetail/fileList/FileList.tsx
@@ -1,6 +1,8 @@
-import { DataTable } from '@/components/data-table'
 import { MeetingFile } from '@/types/meeting.file.type'
 import { columns } from './column'
+import { DataTable } from './data-table'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 interface Props {
     files: MeetingFile[],
@@ -10,9 +12,40 @@ interface Props {
     isLoading: boolean
 }
 
-const FileList = ({files, meetingId, refreshData, currentUserId, isLoading}: Props) => {
+const FileList = ({ files, meetingId, refreshData, currentUserId, isLoading }: Props) => {
+
+    const [typeFilter, setTypeFilter] = useState<string | null>(null);
+    const filteredFiles = typeFilter ? files.filter(file => file.type === typeFilter) : files;
+
+    const uniqueTypes = Array.from(new Set(files.map(file => file.type)));
+
     return (
-        <DataTable isLoading={isLoading} columns={columns(meetingId, refreshData, currentUserId || '')} data={files}/>
+        <div>
+            <div className="flex space-x-2 mb-4">
+                <Button
+                    variant={!typeFilter ? "default" : "outline"}
+                    size="sm"
+                    onClick={() => setTypeFilter(null)}
+                >
+                    All
+                </Button>
+                {uniqueTypes.map((type) => (
+                    <Button
+                        key={type}
+                        variant={typeFilter === type ? "default" : "outline"}
+                        size="sm"
+                        onClick={() => setTypeFilter(typeFilter === type ? null : type)}
+                    >
+                        {type}
+                    </Button>
+                ))}
+            </div>
+            <DataTable
+                isLoading={isLoading}
+                columns={columns(meetingId, refreshData, currentUserId || '')}
+                data={filteredFiles}
+            />
+        </div>
     )
 }
 
